e9bc3ab41c0dba9a0e4105ffd11c8b26c30102c7
Molly Calhoun <mollyfcalhoun@gmail.com>
2024-04-22 13:23:57 -0700

    Add login avatar, fixed link visibility with login, and fixed Home route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const passport = require("passport");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'RDR2 Challenge Tracker' });
+  res.render('home', { title: 'RDR2 Challenge Tracker' });
 });
 
 router.get('/auth/google', passport.authenticate(
