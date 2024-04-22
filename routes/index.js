9f932e03f7640369d0928aa5b6a8817a59c1a5e9
Molly Calhoun <mollyfcalhoun@gmail.com>
2024-04-22 13:08:15 -0700

    Implement OAuth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const passport = require("passport");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'RDR2 Challenge Tracker' });
+});
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    // Optional
+    prompt: 'select_account'
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    // Change to what's best for Your app probably just ROOT '/'
+    failureRedirect: '/'
+  }
+));
+
+router.get('/logout', function(req, res) {
+  req.logout(function() {
+    // Change path for your landing page, probably just ROOT '/'
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
