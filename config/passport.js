d94e916835c221d5154de91980cec23961cfdcf3
Molly Calhoun <mollyfcalhoun@gmail.com>
2024-04-22 23:15:49 -0700

    Update OAuth Credentials, Add Custom Font

--- config/passport.js
+++ config/passport.js
@@ -2,19 +2,25 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const User = require('../models/user');
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK
-}, 
-  // verify callback function
-  // use async/await
+passport.use(new GoogleStrategy(
+  // Configuration object
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+  },
+  // The verify callback function...
+  // Marking a function as an async function allows us
+  // to consume promises using the await keyword
   async function(accessToken, refreshToken, profile, cb) {
-    // a user has logged in with OAuth
+    // When using async/await  we use a
+    // try/catch block to handle an error
     try {
+      // A user has logged in with OAuth...
       let user = await User.findOne({ googleId: profile.id });
-      // Existing user found
+      // Existing user found, so provide it to passport
       if (user) return cb(null, user);
+      // We have a new user via OAuth!
       user = await User.create({
         name: profile.displayName,
         googleId: profile.id,
@@ -27,9 +33,12 @@ passport.use(new GoogleStrategy({
     }
   }
 ));
+
 passport.serializeUser(function(user, cb) {
   cb(null, user._id);
 });
+
 passport.deserializeUser(async function(userId, cb) {
+  // It's nice to be able to use await in-line!
   cb(null, await User.findById(userId));
 });
